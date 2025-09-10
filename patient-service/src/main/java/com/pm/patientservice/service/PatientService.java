dc20d9d06ca554b359307e071d833ddb76522370
Isaiah <ico4@student.le.ac.uk>
2025-09-10 23:39:15 +0100

    EmailAlreadyExists exception & console logs

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -3,6 +3,7 @@ package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -26,7 +27,14 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
-        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A Patient with this email already exists: "
+                    + patientRequestDTO.getEmail());
+        }
+
+        Patient newPatient = patientRepository.save(
+                PatientMapper.toModel(patientRequestDTO));
 
         return PatientMapper.toDTO(newPatient);
     }
